52e923014516e718597ef4919959a6a04b8c2500
Yoav <yoavklein25@gmail.com>
2021-04-26 14:18:28 +0300

    adding http-client repository as submodule

--- curl.sh
+++ curl.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl -v -D - http://kanliveicy.media.kan.org.il/icy/kanbet_mp3 > curl_stream.mp3
+curl -v -D -  http://kanliveicy.media.kan.org.il/icy/kanbet_mp3 > curl_stream.mp3
 
 
